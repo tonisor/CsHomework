fed623a131e8719934ada69493c842e62309d527
Teodor Onisor <onisor_00@yahoo.com>
2017-11-30 20:50:44 +0100

    HW1

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        struct Vec2
+        {
+            public float x, y;
+            public override string ToString()
+            {
+                return "" + x + "," + y;
+            }
+        }
+        struct Circle
+        {
+            public Vec2 center;
+            public float radius;
+            public override string ToString()
+            {
+                return "" + center + ":" + radius;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var circle = new Circle();
+            var point2 = new Vec2();
+            point2.x = point2.y = 3;
+            circle.center = point2;
+
+            var str = circle.ToString();
+
+            Console.WriteLine("{0}", str);
+            Console.ReadKey();
+        }
+    }
+}
